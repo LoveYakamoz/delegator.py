c16e82f4c9f2b92567ea6ad8a31b77bca873d2f7
yangpei3720 <yangpei3720@gmail.com>
2017-09-21 22:55:12 +0800

    Bugfix: 增加测试用例

--- test.py
+++ test.py
@@ -1,8 +1,30 @@
 import delegator
+import unittest
+class WidgetTestCase(unittest.TestCase):
+    '''
+    让所有执行测试的类都继承于TestCase类，
+    可以将TestCase看成是对特定类进行测试的方法的集合 
+    '''
+  
+    def setUp(self):
+        '''在setUp()方法中进行测试前的初始化工作'''
+        pass
 
-if __name__ == '__main__':
-    c = delegator.run(['dir'], block=False)
-    #c = delegator.run(['sleep', '3'], block=False)
-    #c.std_out.write("yagnpei")
+    def tearDown(self):
+        '''
+        并在tearDown()方法中执行测试后的清除工作，
+        setUp()和tearDown()都是TestCase类中定义的方法
+        '''
+        pass
+  
+    def test_delegator_run(self):     
+        c = delegator.run(['sleep', '3'], block=False)
+        c.out
 
-    c.out
+    def test_delegator_dir_run(self):
+        c = delegator.run(['dir'], block=False)
+        c.out
+
+
+if __name__ == "__main__": 
+    unittest.main()
